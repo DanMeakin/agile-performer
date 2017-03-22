d4fd90c2a33482059da12625478c42632117fcfa
HappyFisk <frederiknygart@gmail.com>
2017-03-22 14:06:17 +0100

    Cleaned up initialize file

--- app/js/components/App.jsx
+++ app/js/components/App.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import BarLineChart from './BarLineChart';
+import MultiBarChart from './MultiBarChart';
+import SprintChart from './SprintChart';
+import TeamSkillsChart from './TeamSkillsChart';
+import React, { Component } from 'react';
+import { scrumPractices, xpPractices } from 'js/mocked_data/practices';
+import { teamSkills } from 'js/mocked_data/team_skills';
+import { linesOfCode, testCoverage } from 'js/mocked_data/test';
+import { storyPoints } from 'js/mocked_data/story_points';
+import { happinessIndex } from 'js/mocked_data/happiness_index';
+import { burndown } from 'js/mocked_data/burndown';
+import RGB from 'js/lib/rgb';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div id="content">
-        <h5>Time to <a href="https://facebook.github.io/react/">React</a>.</h5>
-      </div>
-    );
+var testCovOpts = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        callback: val => (
+          (val * 100).toFixed() + "%"
+        ),
+        suggestedMin: 0,
+        suggestedMax: 1,
+        beginAtZero: true
+      }
+    }]
   }
 }
+
+
+
+var colours = [
+  new RGB(238, 64, 53),
+  new RGB(243, 119, 54),
+  new RGB(253, 244, 152),
+  new RGB(123, 192, 67),
+  new RGB(3, 146, 207)
+];
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+      <TeamSkillsChart data={scrumPractices} colours={colours} title="Scrum Practices" />
+      <TeamSkillsChart data={teamSkills} colours={colours} title="Team Skills"/>
+      <SprintChart data={linesOfCode} colours={colours} title="Lines of Code" />
+      <SprintChart data={testCoverage} colours={colours} options={testCovOpts} title="Code Coverage" />
+      <MultiBarChart data={storyPoints} colours={colours} title="Story Points" />
+      <MultiBarChart data={happinessIndex} colours={colours} title="Happiness" />
+      <BarLineChart data={burndown() } colours={colours} title="Burndown" />
+    </div>
+    )
+  }
+}
+
+export default App;
---
+++ app/js/mocked_data/story_points.js
@@ -0,0 +1,23 @@
+
+const storyPoints = [
+  {
+    description: "Commitment",
+    data: {
+      "Sprint 1": 350,
+      "Sprint 2": 280,
+      "Sprint 3": 330,
+      "Sprint 4": 380
+    }
+  },
+  {
+    description: "Work completed",
+    data: {
+      "Sprint 1": 250,
+      "Sprint 2": 280,
+      "Sprint 3": 300,
+      "Sprint 4": 350
+    }
+  }
+]
+
+export { storyPoints };
