614ed61bccab7e2ea6aa600104f60f2633271838
frederik nygart <frederik nygart>
2017-04-07 15:41:47 +0200

    Changed location of metrics coponents

--- app/js/components/metrics/CodeCoverageMetric.jsx
+++ app/js/components/metrics/CodeCoverageMetric.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import PercentageLineChart from './charts/PercentageLineChart';
+import PercentageLineChart from '../charts/PercentageLineChart';
 
 class CodeCoverageMetric extends Component {
   render() {
--- app/js/components/metrics/LinesOfCodeMetric.jsx
+++ app/js/components/metrics/LinesOfCodeMetric.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import LineChart from './charts/LineChart';
+import LineChart from '../charts/LineChart';
 
 class LinesOfCodeMetric extends Component {
   render() {
