3db708fb79cc33e5539d1e59c3342ae16091f11c
Dan Meakin <dan@danmeakin.com>
2017-04-14 16:52:13 +0200

    Add descriptions to practices metrics.
    This commit adds proper descriptions to the scales in the practices metrics
    visualisations.

--- app/js/components/metrics/ScrumPracticesMetric.jsx
+++ app/js/components/metrics/ScrumPracticesMetric.jsx
@@ -4,9 +4,31 @@ import { connect } from 'react-redux';
 import RadarChart from '../charts/RadarChart';
 
 class ScrumPracticesMetric extends Component {
+  chartOptions() {
+    let displayTicks = value => {
+      let adoptionLabels = {
+        1: "Adoption",
+        2: "Adaptation",
+        3: "Acceptance",
+        4: "Routinisation"
+      };
+      return adoptionLabels[value] || "";
+    };
+    return {
+      scale: {
+        ticks: {
+          callback: displayTicks,
+          min: 0,
+          max: 5,
+          stepSize: 1
+        }
+      }
+    }
+  }
+
   render() {
     return (
-      <RadarChart data={this.props.chartData} title="Scrum Practices" />
+      <RadarChart data={this.props.chartData} options={this.chartOptions()} title="Scrum Practices" />
     )
   }
 }
--- app/js/components/metrics/XpPracticesMetric.jsx
+++ app/js/components/metrics/XpPracticesMetric.jsx
@@ -4,9 +4,31 @@ import { connect } from 'react-redux';
 import RadarChart from '../charts/RadarChart';
 
 class XpPracticesMetric extends Component {
+  chartOptions() {
+    let displayTicks = value => {
+      let adoptionLabels = {
+        1: "Adoption",
+        2: "Adaptation",
+        3: "Acceptance",
+        4: "Routinisation"
+      };
+      return adoptionLabels[value] || "";
+    };
+    return {
+      scale: {
+        ticks: {
+          callback: displayTicks,
+          min: 0,
+          max: 5,
+          stepSize: 1
+        }
+      }
+    }
+  }
+
   render() {
     return (
-      <RadarChart data={this.props.chartData} title="XP Practices" />
+      <RadarChart data={this.props.chartData} options={this.chartOptions()} title="XP Practices" />
     )
   }
 }
