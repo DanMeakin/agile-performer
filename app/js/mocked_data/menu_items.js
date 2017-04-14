31d4e35a03ebcd92aa84fcc6ccbd9cf85f858e95
Dan Meakin <dan@danmeakin.com>
2017-04-14 16:52:38 +0200

    Categorise metrics.

--- app/js/mocked_data/menu_items.js
+++ app/js/mocked_data/menu_items.js
@@ -2,86 +2,106 @@ const menuItems = {
   filterTerm: "",
   items: [
     {
-        heading: "Statistics",
+      heading: "Agility",
+      metrics: [
+        {
+          type: "CODE_OWNERSHIP_PROJECT",
+          name: "Code Ownership (Project)"
+        },
+        {
+          type: "CODE_OWNERSHIP_TEAM",
+          name: "Code Ownership (Team)"
+        },
+        {
+          type: "SCRUM_PRACTICES",
+          name: "Scrum Practices"
+        },
+        {
+          type: "SPRINT_INTERFERENCE",
+          name: "Sprint Interference"
+        },
+        {
+          type: "XP_PRACTICES",
+          name: "XP Practices"
+        },
+      ]
+    },
+    {
+      heading: "Codebase",
+      metrics: [
+        {
+          type: "LINES_OF_CODE",
+          name: "Lines of Code"
+        }
+      ]
+    },
+    {
+      heading: "Defects",
+      metrics: [
+        {
+          type: "DEFECTS_OVER_TIME",
+          name: "Defects Over Time"
+        },
+        {
+          type: "REMEDIAL_FOCUS",
+          name: "Remedial Focus"
+        },
+      ]
+    },
+    {
+      heading: "Performance",
+      metrics: [
+        {
+          type: "ENHANCED_RELEASE_BURNDOWN",
+          name: "Enhanced Release Burndown"
+        },
+        {
+          type: "LEAD_TIME",
+          name: "Lead Time"
+        },
+        {
+          type: "SPRINT_BURNDOWN",
+          name: "Sprint Burndown"
+        },
+        {
+          type: "VELOCITY",
+          name: "Velocity"
+        },
+        {
+          type: "VELOCITY_TREND",
+          name: "Velocity Trend"
+        }
+      ]
+    },
+    {
+        heading: "Team Profile",
         metrics: [
-            {
-                type: "CODE_COVERAGE",
-                name: "Code Coverage"
-            } ,
-            {
-                type: "CODE_OWNERSHIP_PROJECT",
-                name: "Code Ownership (Project)"
-            },
-            {
-                type: "CODE_OWNERSHIP_TEAM",
-                name: "Code Ownership (Team)"
-            },
-            {
-                type: "LINES_OF_CODE",
-                name: "Lines of Code"
-            },
-            {
-                type: "TEST_CASES_COUNT",
-                name: "Number of Test Cases"
-            },
-            {
-                type: "REMEDIAL_FOCUS",
-                name: "Remedial Focus"
-            },
-            {
-                type: "TEAM_SATISFACTION",
-                name: "Team Satisfaction"
-            },
-            {
-                type: "VELOCITY",
-                name: "Velocity"
-            },
-            {
-                type: "VELOCITY_TREND",
-                name: "Velocity Trend"
-            },
-            {
-                type: "HAPPINESS_INDEX",
-                name: "Happiness Index"
-            },
-            {
-                type: "ENHANCED_RELEASE_BURNDOWN",
-                name: "Enhanced Release Burndown"
-            },
-            {
-                type: "SPRINT_BURNDOWN",
-                name: "Sprint Burndown"
-            },
-            {
-                type: "SPRINT_INTERFERENCE",
-                name: "Sprint Interference"
-            },
-            {
-                type: "LEAD_TIME",
-                name: "Lead Time"
-            },
-            {
-                type: "DEFECTS_OVER_TIME",
-                name: "Defects Over Time"
-            }
+          {
+            type: "HAPPINESS_INDEX",
+            name: "Happiness Index"
+          },
+          {
+            type: "TEAM_SATISFACTION",
+            name: "Satisfaction"
+          },
+          {
+            type: "TEAM_SKILLS",
+            name: "Skills"
+          }
         ]
     },
     {
-        heading: "Profile",
-        metrics: [
-            {
-                type: "SCRUM_PRACTICES",
-                name: "Scrum Practices"
-            } ,
-            {
-                type: "XP_PRACTICES",
-                name: "XP Practices"
-            },
-            {
-                type: "TEAM_SKILLS",
-                name: "Team Skills"
-            }
-        ]
+      heading: "Testing",
+      metrics: [
+        {
+          type: "CODE_COVERAGE",
+          name: "Code Coverage"
+        },
+        {
+          type: "TEST_CASES_COUNT",
+          name: "Number of Test Cases"
+        }
+      ]
     }
   ]
 };
