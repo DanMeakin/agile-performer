ecfe1591b7c580b5c63c4cb85f031e6c2326ef5b
Dan Meakin <dan@danmeakin.com>
2017-04-22 13:57:06 +0200

    Order metrics imports; remove unused metrics components.

--- app/js/components/metrics/index.js
+++ app/js/components/metrics/index.js
@@ -1,31 +1,31 @@
-import TeamSatisfactionMetric from './TeamSatisfactionMetric';
-import VelocityLineMetric from './VelocityLine';
-import VelocityBarMetric from './VelocityBar';
-import HappinessIndexMetric from './HappinessIndexMetric';
+import DefectsOverTimeMetric from './DefectsOverTime';
 import EnhancedReleaseBurndownMetric from './EnhancedReleaseBurndown';
+import HappinessIndexMetric from './HappinessIndexMetric';
+import ProjectCodeOwnershipMetric from './ProjectCodeOwnership';
+import RemedialFocusMetric from './RemedialFocus';
+import ScrumPracticesMetric from './ScrumPracticesMetric';
 import SprintBurndownBarMetric from './SprintBurndownBar';
 import SprintBurndownLineMetric from './SprintBurndownLine';
-import ScrumPracticesMetric from './ScrumPracticesMetric';
-import XpPracticesMetric from './XpPracticesMetric';
-import ProjectCodeOwnershipMetric from './ProjectCodeOwnership';
-import TeamCodeOwnershipMetric from './TeamCodeOwnership';
-import RemedialFocusMetric from './RemedialFocus';
 import SprintInterferenceMetric from './SprintInterference';
-import DefectsOverTimeMetric from './DefectsOverTime';
+import TeamCodeOwnershipMetric from './TeamCodeOwnership';
+import TeamSatisfactionMetric from './TeamSatisfactionMetric';
+import VelocityBarMetric from './VelocityBar';
+import VelocityLineMetric from './VelocityLine';
+import XpPracticesMetric from './XpPracticesMetric';
 
 export {
+  DefectsOverTimeMetric,
+  EnhancedReleaseBurndownMetric,
+  HappinessIndexMetric,
+  ProjectCodeOwnershipMetric,
+  RemedialFocusMetric,
+  ScrumPracticesMetric,
+  SprintBurndownBarMetric,
+  SprintBurndownLineMetric,
+  SprintInterferenceMetric,
+  TeamCodeOwnershipMetric,
   TeamSatisfactionMetric,
   VelocityBarMetric,
   VelocityLineMetric,
-  HappinessIndexMetric,
-  EnhancedReleaseBurndownMetric,
-  SprintBurndownBarMetric,
-  SprintBurndownLineMetric,
-  ScrumPracticesMetric,
-  XpPracticesMetric,
-  ProjectCodeOwnershipMetric,
-  TeamCodeOwnershipMetric,
-  RemedialFocusMetric,
-  SprintInterferenceMetric,
-  DefectsOverTimeMetric
+  XpPracticesMetric
 };
