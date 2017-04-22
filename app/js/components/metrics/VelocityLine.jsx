2aa8c42aa8f55e221ed3ad89e331924fd2427a45
Dan Meakin <dan@danmeakin.com>
2017-04-22 11:56:55 +0200

    Create Velocity line chart.

---
+++ app/js/components/metrics/VelocityLine.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import LineChart from '../charts/LineChart';
+
+class VelocityMetric extends Component {
+  chartData() {
+    return this.props.chartData.map(({ description, data }) => (
+      {
+        description,
+        data,
+        chartType: "line",
+        borderDash: description == "Commitment" ? [10, 5] : undefined
+      }
+    ))
+  }
+
+  render() {
+    return (
+        <LineChart data={this.chartData()} title="Velocity" />
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    chartData: state.metrics.velocity
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(VelocityMetric);
