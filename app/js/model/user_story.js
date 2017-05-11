6e5f4f3ac4d5e9005de01752e4dd57581f9bc2db
Dan Meakin <dan@danmeakin.com>
2017-05-11 11:32:24 +0200

    Add isAdditional flag to user story.
    This flags whether a story represents an additional to a release, added after
    planning was conducted, i.e. whether it is additional to scope.

--- app/js/model/user_story.js
+++ app/js/model/user_story.js
@@ -6,11 +6,14 @@ export default class UserStory {
    * @param {integer} storyPoints - The number of story points for this story
    * @param {date} dateAdded - The date this story was added to a sprint
    * @param {date} dateDone - The date this story was complete
+   * @param {boolean} isAdditional - Flag whether this story is added to a
+   * release post-planning, i.e. is additional to scope
    */
-  constructor(description, storyPoints, dateAdded, dateDone) {
+  constructor(description, storyPoints, dateAdded, dateDone, isAdditional) {
     this.description = description;
     this.storyPoints = storyPoints;
     this.dateAdded = dateAdded;
     this.dateDone = dateDone;
+    this.isAdditional = isAdditional;
   }
 };
