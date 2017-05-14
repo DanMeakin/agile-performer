3bf9088c1c2536d542c822a7e9142c1bcc28e696
Dan Meakin <dan@danmeakin.com>
2017-05-14 10:57:01 +0200

    Add breadcrumb links to Product Tracking.

--- app/js/components/dashboards/product_tracking/ProductTracking.jsx
+++ app/js/components/dashboards/product_tracking/ProductTracking.jsx
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import ReleaseBurnup from './ReleaseBurnup';
+import Dashboard from '../../dashboards';
 import OverviewTable from './OverviewTable';
+import Breadcrumb from '../../Breadcrumb';
 
 class ProductTracking extends Component {
   render() {
+    let breadcrumbLinks = [
+      {
+        viewName: Dashboard,
+        label: "Home"
+      },
+      {
+        viewName: ProductTracking,
+        label: "Product Tracking"
+      }
+    ];
     return (
       <div className="product-tracking-dashboard">
+        <Breadcrumb links={breadcrumbLinks} />
         <h1>Product Tracking</h1>
         <div className="row">
           <div className="medium-9 columns">
