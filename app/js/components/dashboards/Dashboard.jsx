5927cb6f94e8836cd18762062fcf06cd24f4dde6
Dan Meakin <dan@danmeakin.com>
2017-05-15 17:18:20 +0200

    Add title, breadcrumb to Dashboard.

--- app/js/components/dashboards/Dashboard.jsx
+++ app/js/components/dashboards/Dashboard.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import DashboardTitle from '../DashboardTitle';
+import Breadcrumb from '../Breadcrumb';
 
 export default class Dashboard extends Component {
   render() {
+    let breadcrumbLinks = [
+      {
+        label: "Home"
+      }
+    ];
     return (
-      <div></div>
+      <div>
+        <Breadcrumb links={breadcrumbLinks} />
+        <DashboardTitle>Agile Dashboard</DashboardTitle>
+      </div>
     )
   }
 }
