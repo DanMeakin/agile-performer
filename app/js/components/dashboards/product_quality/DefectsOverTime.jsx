f3b4995c4a43dfd145bff7fae46d30086bc2ec58
HappyFisk <frederiknygart@gmail.com>
2017-05-15 20:34:05 +0200

    cahnged to filled line chart

--- app/js/components/dashboards/product_quality/DefectsOverTime.jsx
+++ app/js/components/dashboards/product_quality/DefectsOverTime.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import LineChart from '../../charts/LineChart';
+import FilledLineChart from '../../charts/FilledLineChart';
 
 class DefectsOverTime extends Component {
   render() {
     return (
-      <LineChart data={this.props.chartData} options={this.props.options} title="Current Defects" />
+      <FilledLineChart data={this.props.chartData} options={this.props.options} title="Current Defects" />
     )
   }
 }
